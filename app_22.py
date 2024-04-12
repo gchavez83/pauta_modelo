55c49187379ba8c327d130806e5f32cb1e2e1302
gchavez83 <61848325+gchavez83@users.noreply.github.com>
2024-04-12 16:52:16 -0600

    Update app_22.py
    web

--- app_22.py
+++ app_22.py
@@ -19,23 +19,20 @@ st.title("Modelo de Predicción de Resultados de Anuncios")
 st.markdown("Elija los valores para pronosticar el Resultado del Anuncio)")
 
 form = st.form("anuncios")
-Importe_invertido = form.number_input('Importe Invertido', min_value = 0.0 , max_value = 20000.00,value=2000.00 , format = '%.2f', step = 1)
+importe_gastado = form.number_input('Importe', min_value = 0.0 , max_value = 20000.00,value=2000.00 , format = '%.2f', step = 1)
 clasificacion_list = ['Atributos','Especiales','Estratégicas','Estratégico','Spots','Territorio']
-Clasificacion_Descripcion = form.selectbox('Clasificacion', clasificacion_list)
+clasificacion_descripcion = form.selectbox('Clasificacion', clasificacion_list)
 objetivo_list = ['Interacción','ThruPlay']
-Objetivo_Descripcion = form.selectbox('Objetivo', objetivo_list)
+objetivo_descripcion = form.selectbox('Objetivo', objetivo_list)
 redsocial_list = ['Dark Post', 'Facebook','Instagram']
-RedSocial_Descripcion = form.selectbox('Red Social', redsocial_list)
-#Cuadrante_list = ['NE', 'NO', 'SE', 'SO']
-#Cuadrante = form.selectbox('Cuadrante', Cuadrante_list)
-#Distance_Km = form.slider('Distance Km', min_value = 0.0 , max_value = 15.0 , value = 2.0, format = '%.2f' )
+redsocial_descripcion = form.selectbox('Red Social', redsocial_list)
 
 predict_button = form.form_submit_button('Predict')
 
-input_dict = {'Importe gastado (MXN)': Importe_invertido, 'Clasificacion.Descripción': Clasificacion_Descripcion, 'Objetivo.Descripción': Objetivo_Descripcion, 'Red_Social.Descripción': RedSocial_Descripcion}
+input_dict = {'importe_gastado': importe_gastado, 'clasificacion_descripcion': clasificacion_descripcion, 'objetivo_descripcion': objetivo_descripcion, 'redsocial_descripcion': redsocial_descripcion}
 
 input_df = pd.DataFrame([input_dict])
 
 if predict_button: 
     out = predict(model, input_df)
-    st.success(f'La prediccion del predio es {out}.')
+    st.success(f'La prediccion es {out}.')
